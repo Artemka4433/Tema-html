6b90aeed64b767722d81bf1482537bafc0494a98
Vel <avior@gmail.com>
2020-03-11 12:01:20 +0200

    new

--- index.php
+++ index.php
@@ -2,25 +2,41 @@
 <html>
 <head>
 <title>
-EOSJacks
+EOSJacks info
 </title>
 </head>
 <body bgcolor="#4B0082">
 
-   
+<table>   
   <center>
 	<p><font size=7 color=red face=Impact >EOSJacks</font><p>
-	<p><font size=5 color=#8d416c face=Calibri >Игровая платформа</font></p>
+	<p><font size=5 color=#8d416c face=Calibri >Контактная информация</font></p>
   </center>
+  <font size=4 face=Calibri color=#da962d > Запуская игру в первый раз, вы рассчитываете на то, что она оправдает каждую потраченную на нее копейку. Наша цель – оправдать эти ожидания. Мы не можем гарантировать, что вы никогда не встретите ошибки (как бы усердно мы ни работали), но мы можем гарантировать, что позаботимся об этом, если что-то пойдет не так.</font> 
+ <p align=right> <font size=4 face=ArialNarrow color=#da962d > Черепаха.А</font></p> 
 
-<center><nav><a href="https://www.yakaboo.ua/ru/"><font color=red>Главная</font></a>|<a href="https://www.yakaboo.ua/knigi.html"><font color=red>Новинки</font></a>|<a href="https://www.yakaboo.ua/knigi.html"><font color=red>Каталог</font></a>|<a href="https://www.yakaboo.ua/payment/"><font color=red>Доставка</font></a>|<a href="laba1.html"><font color=red>Контакты</font></a></nav></center>
+<h1><font color=red  >Адрес</font></h1>
 	
- <p style="margin-top:30px"> <font size=4 face=Calibri color=#da962d > EOSJacks - это платформа для онлайн-игр, которая позволяет загружать новейшие из мира видеоигр по мере их выпуска - незаменимый инструмент, принятый многими издателями. Это означает, что нет никаких взломов.</font> </p>
- <p style="margin-top:30px"> <font size=4 face=Calibri color=#da962d > EOSJacks предлагает около 30 000 игр на любой вкус, а также эксклюзивные предложения, автоматическое обновление игр и другие замечательные возможности.</font> </p>
-<center>
-<img src="EOSJacks.png" width="630" height="500" alt="Игровая платформа" title="Игровая платформа"/>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/VERbfWgh8F0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</center>
+	<p style="margin-left: 40px";><font size=3 color=#da962d>пр.Пушкина</font></p>
+
+<h1><font  color=red  >Телефон</font></h1>
+	
+	<p style="margin-left: 40px";><font size=3 color=#da962d>+38(050)1234567</font></p>
+	
+<h1><font  color=red  >Факс</font></h1>
+
+	<p style="margin-left: 40px";><font size=3 color=#da962d>+38(050)7654321</font></p>
+
+<h1><font  color=red  >E-mail</font></h1>
+	
+	<p style="margin-left: 40px";><font size=3 color=#da962d>pizzeria@gmail.com</font></p>
+
+<h1><font  color=red  >Сайт</font></h1>
+	
+	<p style="margin-left: 40px";><font size=3 color=#da962d>www.pizzeria.com.ua</font></p>
+	
+</table>   
+
   
 </body>
 </html>
