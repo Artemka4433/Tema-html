80cb712dd7170259c5140152ecc78b525f8ba5f4
Vel <avior@gmail.com>
2020-03-11 12:03:49 +0200

    new

--- index.php
+++ index.php
@@ -2,41 +2,82 @@
 <html>
 <head>
 <title>
-EOSJacks info
+EOSJacks
 </title>
-</head>
-<body bgcolor="#4B0082">
+	</head>
+	<body bgcolor="#4B0082">
 
-<table>   
-  <center>
-	<p><font size=7 color=red face=Impact >EOSJacks</font><p>
-	<p><font size=5 color=#8d416c face=Calibri >Контактная информация</font></p>
-  </center>
-  <font size=4 face=Calibri color=#da962d > Запуская игру в первый раз, вы рассчитываете на то, что она оправдает каждую потраченную на нее копейку. Наша цель – оправдать эти ожидания. Мы не можем гарантировать, что вы никогда не встретите ошибки (как бы усердно мы ни работали), но мы можем гарантировать, что позаботимся об этом, если что-то пойдет не так.</font> 
- <p align=right> <font size=4 face=ArialNarrow color=#da962d > Черепаха.А</font></p> 
+	<table border="1" cellpadding="7" cellspacing="0"> 
+    <td colspan="2" bgcolor="#D3EDF6" align="center"><font size=6> EOSJacks</font></td>     
+   <tr>
+    <td><a href="https://www.yakaboo.ua/ru/"><font color=red>Главная</font></a><br>
+<a href="https://www.yakaboo.ua/knigi.html"><font color=red>Новинки</font></a><br>
+<a href="https://www.yakaboo.ua/knigi.html"><font color=red>Каталог</font></a><br>
+<a href="https://www.yakaboo.ua/payment/"><font color=red>Доставка</font></a><br>
+<a href="laba1.html"><font color=red>Контакты</font></a></nav></center> 
+    <td><h1 align="center"><font color="#4B0082" face="consolas">Форма заказа</font></h1>
+					<p><font size="4" face="consolas">Для регистрации заполните следующие поля.</font></p>
+					<p><b><font size="4" color="red" face="consolas">Примечание:</font></b><font size="4"face="consolas">поля помеченные</font><font color="red"> * </font><font size="4" face="consolas">обзательны для заполнения</font></p>
+					</td>	
+				</tr>
+				<tr bgcolor="#4B0082">	
+				<td width="30%" height="50%" valign="top">
+				<form name="books" method="post" action="successs.html">
+					<p align=center><font size=5 face="Times New Roman" color=red>Легендарная арена созданая лучшим киберспортсменома мира S1mple </font></p>
+    <p align=center><img src="Alex.jpg" alt="Произошел мем" width="225" height="225" title="Картинка 1"></p>
+    <p align=center style="margin-left: 200px"><font face="Verdana" size=3>Александр Костылев</font></p>
+					<td>
+					<p>
+					<font size="4" face="consolas">Имя:</font><input style = "margin-left:95px"type="text" min="3" required="1" value=""><font color="red">*</font>
+					</p>
+					<p>
+						<font size="4" face="consolas">Фамилия:<input style = "margin-left:65px"type="text" min="6" required="1" value=""><font color="red">*</font>
+					</p>
+					<p>
+						<font size="4" face="consolas">Логин:<input style = "margin-left:45px"type="text" min="1" required="1" value=""><font color="red">*</font>
+					</p>
+					<p>
+						<font size="4" face="consolas">Пароль:<input style = "margin-left:5px"type="text" min="5" required="1" value=""><font color="red">*</font>
+					</p>
+					<p>
+						<font size="4" face="consolas">Способ оплаты<font color="red">*</font>
+					</p>
+					<p>
+						<font size="4" face="consolas"><input style = "margin-left:5px"type="radio"   value="posilka" name="delivery" required="1"/>  Наличные
+					</p>
+					<p>
+						<font size="4" face="consolas"><input style = "margin-left:5px" type="radio" name="delivery" value="peshkom"/>  Картой
+					</p>
+					<p>
+						<font size="4" face="consolas">Способ оплаты<font color="red">*</font>
+					</p>
+					<select>
+						<option>"Альфа Банк"</option>
+						<option>"Банк Аваль"</option>
+						<option> "Приват Банк"</option>
+						<option> "Монобанк"</option>
+					</select>
+					<form name="comment" method="post">
+					  <p>
+						<label>Комментарий:</label>
+						<br/>
+						<textarea name="text_comment" cols="30" rows="3"></textarea>
+					  </p>
+					  <p>
+						<input type="hidden" name="page_id" value="150" />
+						<input type="submit" value="Отправить" />
+						 <p>Ознакомлен с условиями доставки<input type="checkbox" required="1" name="option1"><Br>
+					  </p>
+					</form>
+					</style>
+					</form>
+				</td>
+   </tr>
+   <tr>
+   
+   </tr>
+    <td colspan="2" bgcolor="#D3EDF6" align="center"><font size 4>Остались вопросы? Пришлите нам письмо: </font><a href="#"><font size=4>EOSJacks@gmail.com</font></td>
+  </table>
 
-<h1><font color=red  >Адрес</font></h1>
-	
-	<p style="margin-left: 40px";><font size=3 color=#da962d>пр.Пушкина</font></p>
-
-<h1><font  color=red  >Телефон</font></h1>
-	
-	<p style="margin-left: 40px";><font size=3 color=#da962d>+38(050)1234567</font></p>
-	
-<h1><font  color=red  >Факс</font></h1>
-
-	<p style="margin-left: 40px";><font size=3 color=#da962d>+38(050)7654321</font></p>
-
-<h1><font  color=red  >E-mail</font></h1>
-	
-	<p style="margin-left: 40px";><font size=3 color=#da962d>pizzeria@gmail.com</font></p>
-
-<h1><font  color=red  >Сайт</font></h1>
-	
-	<p style="margin-left: 40px";><font size=3 color=#da962d>www.pizzeria.com.ua</font></p>
-	
-</table>   
-
-  
 </body>
 </html>
