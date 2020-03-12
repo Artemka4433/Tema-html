ecf6f4f7c51de342765c7c58cd0cfacf4f249351
Vel <avior@gmail.com>
2020-03-12 13:02:41 +0200

    new

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <title>
 EOSJacks
 </title>
+<script src="script.js">
+</script>
 	</head>
 	<body bgcolor="#4B0082">
 
@@ -22,22 +24,22 @@ EOSJacks
 				</tr>
 				<tr bgcolor="#4B0082">	
 				<td width="30%" height="50%" valign="top">
-				<form name="books" method="post" action="successs.html">
+				
 					<p align=center><font size=5 face="Times New Roman" color=red>Легендарная арена созданая лучшим киберспортсменома мира S1mple </font></p>
     <p align=center><img src="Alex.jpg" alt="Произошел мем" width="225" height="225" title="Картинка 1"></p>
     <p align=center style="margin-left: 200px"><font face="Verdana" size=3>Александр Костылев</font></p>
 					<td>
 					<p>
-					<font size="4" face="consolas">Имя:</font><input style = "margin-left:95px"type="text" min="3" required="1" value=""><font color="red">*</font>
+					<font size="4" face="consolas">Имя:</font><input id="firstname"style = "margin-left:95px"type="text" min="3" required="1" value=""><font color="red">*</font>
 					</p>
 					<p>
-						<font size="4" face="consolas">Фамилия:<input style = "margin-left:55px"type="text" min="6" required="1" value=""><font color="red">*</font>
+						<font size="4" face="consolas">Фамилия:<input id="lastname"style = "margin-left:55px"type="text" min="6" required="1" value=""><font color="red">*</font>
 					</p>
 					<p>
-						<font size="4" face="consolas">Логин:<input style = "margin-left:75px"type="text" min="1" required="1" value=""><font color="red">*</font>
+						<font size="4" face="consolas">Логин:<input id="login"style = "margin-left:75px"type="text" min="1" required="1" value=""><font color="red">*</font>
 					</p>
 					<p>
-						<font size="4" face="consolas">Пароль:<input style = "margin-left:65px"type="text" min="5" required="1" value=""><font color="red">*</font>
+						<font size="4" face="consolas">Пароль:<input id="password"style = "margin-left:65px"type="text" min="5" required="1" value=""><font color="red">*</font>
 					</p>
 					<p>
 						<font size="4" face="consolas">Способ оплаты<font color="red">*</font>
@@ -57,20 +59,20 @@ EOSJacks
 						<option> "Приват Банк"</option>
 						<option> "Монобанк"</option>
 					</select>
-					<form name="comment" method="post">
+					
 					  <p>
 						<label>Комментарий:</label>
 						<br/>
-						<textarea name="text_comment" cols="30" rows="3"></textarea>
+						<textarea id="comment" name="text_comment" cols="30" rows="3"></textarea>
 					  </p>
 					  <p>
 						<input type="hidden" name="page_id" value="150" />
-						<input type="submit" value="Отправить" />
+						<input type="button" value="Отправить" onclick="sent()" />
 						 <p>Ознакомлен с условиями доставки<input type="checkbox" required="1" name="option1"><Br>
 					  </p>
-					</form>
+					
 					</style>
-					</form>
+					
 				</td>
    </tr>
    <tr>
